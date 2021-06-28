90709cd63f76e27857da2cd586047097d72962fd
Sherine Darwish <sheriinekhaled97@gmail.com>
2021-06-28 23:35:04 +0200

    roll back

--- app.js
+++ app.js
@@ -63,5 +63,5 @@ app.use("/users", userRouter);
 
 module.exports = app;
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, console.log(`Server running on ${PORT}`));
+ const PORT = process.env.PORT || 3000;
+ app.listen(PORT, console.log(`Server running on ${PORT}`));
