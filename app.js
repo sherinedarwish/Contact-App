9fa519355966581ce985bb61e6f4ee3b218de561
Sherine Darwish <sheriinekhaled97@gmail.com>
2021-07-01 18:12:25 +0200

    updating

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 var path = require("path");
 const mongoose = require("mongoose");
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var createError = require('http-errors');
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/users");
@@ -22,8 +25,10 @@ app.set("view engine", "ejs");
 app.set("layout", "./layout");
 
 // Body Parser
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());app.use(express.static(path.join(__dirname, 'public')));
 
 // Express session
 app.use(
@@ -61,6 +66,22 @@ mongoose
 app.use("/", indexRouter);
 app.use("/users", userRouter);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
 module.exports = app;
 
 //const PORT = process.env.PORT || 3000;
