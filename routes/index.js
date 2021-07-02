48d9d3868360bb15bd336afad82e7e638f48fc5e
Andrew Magdy <andrewmagdygad@gmail.com>
2021-07-02 11:04:07 +0200

    fix: edit contact

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const createcontact = require("../services").createcontact;
 const getcontacts = require("../services").getcontacts;
 const getcontactsByUser = require("../services").getcontactsByUser;
-const editcontact = require("../services").editcontact;
+const getcontact = require("../services").getcontact;
 const deletecontact = require("../services").deletecontact;
 const Contact = require("../models/Contact");
 const { ensureAuthenticated } = require("../config/auth");
@@ -15,38 +15,34 @@ router.get("/", (req, res, next) => {
 
 // View All Page
 router.get("/viewall", ensureAuthenticated, async (req, res) => {
-  const data = await getcontactsByUser(req);
-  console.log("data from viewall= ",data);
-  res.render('viewall',{data: data})
+    const data = await getcontactsByUser(req);
+    console.log("data from viewall= ", data);
+    res.render("viewall", { data: data });
 });
 
 // Dashboard page
 router.get("/dashboard", ensureAuthenticated, (req, res) =>
-  res.render("dashboard", {
-    name: req.user.name
-}));
-
+    res.render("dashboard", {
+        name: req.user.name,
+    })
+);
 
 // POST METHOD
-router.post('/dashboard', async function(req, res, next) {
-    await createcontact(req,res);
-    res.render('dashboard',{name: req.user.name });
+router.post("/dashboard", async function (req, res, next) {
+    await createcontact(req, res);
+    res.render("dashboard", { name: req.user.name });
 });
 
 // Edit
 router.get("/edit/:id", ensureAuthenticated, async (req, res) => {
-  const data = await editcontact(req);
-  console.log("data from editing= ",data);
-  const { first_name, last_name, number } = data;
-  console.log("first_name: ", first_name);
-  res.render('edit',{data})
+    const data = await getcontact(req);
+    console.log("data from editing= ", data);
+    res.render("edit", { ...data._doc });
 });
 
 // delete
 router.get("/delete/:id", ensureAuthenticated, async (req, res) => {
-  await deletecontact(req,res);
-  
+    await deletecontact(req, res);
 });
-
 
 module.exports = router;
--- services.js
+++ services.js
@@ -1,61 +1,62 @@
 const Contact = require("./models/Contact");
-const User = require('./models/User');
+const User = require("./models/User");
 
 //POST METHOD
-async function createcontact(req,res){
+async function createcontact(req, res) {
     const userId = req.user._id;
-    const { first_name,last_name,number } = req.body;
+    const { first_name, last_name, number } = req.body;
 
     const newContact = new Contact({
         first_name,
         last_name,
         number,
-        userId
+        userId,
     });
-    
 
     newContact
         .save()
-        .then(contact => {
-            console.log("New Contact Saved")})
-        .catch(err => console.log(err))
-};
-
+        .then((contact) => {
+            console.log("New Contact Saved");
+        })
+        .catch((err) => console.log(err));
+}
 
 // GET METHOD
-async function getcontacts(req,res){
-    const data = await Contact.find().catch(err=> console.error(err));
-    return data
-};
-
+async function getcontacts(req, res) {
+    const data = await Contact.find().catch((err) => console.error(err));
+    return data;
+}
 
 // GET CONTACTS FROM ID
-async function getcontactsByUser(req){
+async function getcontactsByUser(req) {
     const userID = req.user._id;
-    const data = await Contact.find({"userId": userID}).catch(err=> console.error(err));
-    return data
-};
-
+    const data = await Contact.find({ userId: userID }).catch((err) =>
+        console.error(err)
+    );
+    return data;
+}
 
 // Delete Method
-async function deletecontact(req,res){
-    Contact.findByIdAndRemove(req.params.id).then(data => console.log(data)).catch(err=> console.error(err));
-    res.redirect('/viewall');
- 
-};
-
+async function deletecontact(req, res) {
+    Contact.findByIdAndRemove(req.params.id)
+        .then((data) => console.log(data))
+        .catch((err) => console.error(err));
+    res.redirect("/viewall");
+}
 
 // Update
-async function editcontact(req){
+async function getcontact(req) {
     const contactId = req.params.id;
-    const data = await Contact.find({"_id": contactId}).catch(err=> console.error(err));
-    return data
-};
+    const data = await Contact.findById(contactId).catch((err) =>
+        console.error(err)
+    );
+    return data;
+}
 
 module.exports = {
     createcontact,
     getcontacts,
     deletecontact,
     getcontactsByUser,
-    editcontact
-}
+    getcontact,
+};
